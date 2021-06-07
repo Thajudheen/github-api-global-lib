3185f39dedd5e0a128a9a041b049836c4781f9ae
Thaj <hellothaj@gmail.com>
2021-06-07 17:19:07 +0200

    Pass map

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,3 @@
-def call (String name, String dayOfWeek) {
-    sh "echo Hello ${name}. Today is ${dayOfWeek}."
+def call (Map config = [:]) {
+    sh "echo Hello ${config.dayOfWeek}. Today is ${config.name}."
 }
